9da1cf4ce66647ec28d57b55948eb0e866798b4a
James Cape <james.cape@gmail.com>
2019-08-25 22:48:26 -0600

    Removes staging knex setup and adds seeds directory path
    Co-Authored-By: Brian Plantico 
    <43261385+bplantico@users.noreply.github.com>

--- knexfile.js
+++ knexfile.js
@@ -14,27 +14,30 @@ module.exports = {
     useNullAsDefault: true
   },
 
-  staging: {
-    client: 'pg',
-    connection: {
-      database: 'my_db',
-      user:     'jamescape',
-      password: null
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'recipes'
-    }
-  },
+  // staging: {
+  //   client: 'pg',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'jamescape',
+  //     password: null
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'recipes'
+  //   }
+  // },
 
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL + `?ssl=true`,
     migrations: {
       directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
     },
     useNullAsDefault: true
   }
