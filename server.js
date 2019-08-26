54090140e2077a18b03b80abbbe12484e6849166
James Cape <james.cape@gmail.com>
2019-08-25 21:42:13 -0600

    Adds /api/v1/recipes route to return all recipes in the seeds database
    Co-Authored-By: Brian Plantico 
    <43261385+bplantico@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
 app.set('port', process.env.PORT || 3000);
-app.locals.title = 'Pet Box';
+app.locals.title = 'Edamame Service';
 
 app.get('/', (request, response) => {
-  response.send('Oh hey Pet Box');
+  response.send('This is a microservice for CalorieCombs');
+});
+
+app.get('/api/v1/recipes', (request, response) => {
+  database('recipes').select()
+    .then((recipes) => {
+      response.status(200).json(recipes);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
 });
 
 app.listen(app.get('port'), () => {
