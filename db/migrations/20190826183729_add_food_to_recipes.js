74d5097e0b240527bc376eea5cee36ac95bce92b
Brian Plantico <bplantico@gmail.com>
2019-08-27 11:23:59 -0600

    IN PROGRESS adds food column migration

--- db/migrations/20190826183729_add_food_to_recipes.js
+++ db/migrations/20190826183729_add_food_to_recipes.js
@@ -1,8 +1,11 @@
-
-exports.up = function(knex) {
-  
+exports.up = function(knex, Promise) {
+    return knex.schema.table('recipes', function(t) {
+        t.string('food').notNull().defaultTo(0);
+    });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function(knex, Promise) {
+    return knex.schema.table('recipes', function(t) {
+        t.dropColumn('food');
+    });
 };
